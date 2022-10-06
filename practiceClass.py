7914a14f777cbb0527811d9ac6f49be6d6cf4318
Avery <sherlock@holmes.com>
2022-10-06 10:25:08 -0500

    coding

--- practiceClass.py
+++ practiceClass.py
@@ -1,14 +1,49 @@
 '''Create two class definitions
 
-1) a Play class that has attributes for
-id, name, number of seats, date and status. Set the value of
-the status attribute to True as default. Create an accessor
-method each for the name, seats and status attributes only. 
-Create a method called 'seats_left' that will reduce the 
-number of seats by 1 every time it is called.
-Create a mutator method called 'set_status' that will
-change the status attribute to False.
+1) a Play class that has attributes for id, name, number of seats, date and status. ✅
+Set the value of the status attribute to True as default. 
+Create an accessor method each for the name, seats and status attributes only. ✅
+Create a method called 'seats_left' that will reduce the number of seats by 1 every time it is called.
+Create a mutator method called 'set_status' that will change the status attribute to False.
 
-2) a Booking class that has attributes for customer name and
-seat number. Create accessor methods for both attributes.'''
+2) a Booking class that has attributes for customer name and seat number. ✅
+Create accessor methods for both attributes. ✅''' 
         
+class Play:
+    def __init__(self, iD, name, seats, date):
+        self.__iD = iD
+        self.__name = name
+        self.__seats = seats
+        self.__date = date
+        self.__status = True
+
+    def get_name(self):
+        return self.__name
+        
+    def get_seats(self):
+        #self.seats = number
+        return self.__seats 
+
+    def get_status(self):
+        return self.__status
+
+    def seats_left(self):
+        self.__remain = self.__seats - 1
+
+    def get_seats_left(self):
+        return self.__remain
+
+    def set_status(self):
+        self.__status = False
+
+class Booking:
+    def __init__(self, name, seat_num):
+        self.__name = name
+        self.__seat_num = seat_num
+
+    def get_name(self):
+        return self.__name
+    
+    def get_seat_num(self):
+        return self.__seat_num
+
--- practiceProgram.py
+++ practiceProgram.py
@@ -1,4 +1,5 @@
-import practiceClassKEY as p
+#import practiceClassKEY as p
+import practiceClass as pc
 import csv
 
 
@@ -34,6 +35,22 @@ shows = {
 play class that has id 9587
 NOTE: Do not hard code the values to create the instance but use
 keys and values from the dictionary '''
+#instance = pc.Play(9587)
+dictionary = {}
+
+for key in shows:
+    if shows[key]["id"] == 9587:
+        #iD = shows[key]["id"]
+        #name = shows[key]["name"]
+        #seats = shows[key]["capacity"]
+        #date = shows[key]["event_date"]
+        #instance = pc.Play(iD, name, seats, date)
+        #print("yes")
+        instance = pc.Play(shows[key]["id"],shows[key]["name"],shows[key]["capacity"],shows[key]["event_date"])
+        #dictionary[instance.get_iD()] = [instance.get_name()]
+
+#print(dictionary)
+
 
 
 
@@ -46,9 +63,49 @@ error message as shown in output.JPG'''
 
 
 #open the csv file in read mode
+infile = open("bookings.csv","r")
+csvfile = csv.reader(infile, delimiter = ",")
+next(csvfile)
+open_seats = 124
+num = 1
+
+#for line in csvfile:
+#    print(line)
+
+avail_seats = instance.get_seats()
+
+for line in csvfile:
+    if float(line[0]) == 9587:
+        avail_seats = avail_seats - 1
+        #instance.seats_left(124)
+        #print(instance.get_move())
+        if avail_seats > -1:
+            continue
+        else:
+            print("*********ERROR*********\n")
+            print("Guest Name: " + line[1] + "\n")
+            print("Sorry, show: Hamilton is sold out \n")
+
+
+
+        
+
+
+'''
+print(avail_seats)
+
+for records in csvfile:
+    if float(records[0]) == 9587:
+        avail_seats-=1
+        if avail_seats > 0:
+            print("Welcome")
+        else:
+            print("*********ERROR*********\n" + "GuestName: " + records[1] + "\n" + "Sorry, show: Hamilton is sold out")
+'''
 
 
 #create a csv object from the file object from the step above
+#if instance[]
 
 
 
